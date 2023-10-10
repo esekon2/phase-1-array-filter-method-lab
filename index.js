a959da887f42592dd7108d3dba4eec30406c5241
densel.esekon2@student.moringaschool.com <esekondensel@gmail.com>
2023-10-10 17:17:56 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+// From the test we need to define findMatching function
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  }
+  
+  // From the test we need to define fuzzyMatch function
+  function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(letters.toLowerCase()));
+  }
+  
+  // From the test we need to definematchName function
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+  }
+  
