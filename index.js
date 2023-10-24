24830d7a20eedef3c8ff6cfaa2866207a15ab1f1
densel.esekon2@student.moringaschool.com <esekondensel@gmail.com>
2023-10-24 21:24:56 +0300

    Assignment Resubmitted

--- index.js
+++ index.js
@@ -1,16 +1,60 @@
 // Code your solution here
 // From the test we need to define findMatching function
-function findMatching(drivers, name) {
-    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+function findMatching(drivers, dname) {
+  for (let i=0; i < drivers.length;){
+      const newMatch =[];
+      const match = drivers.filter(element => {
+          if (element.toUpperCase() === dname.toUpperCase()){
+              newMatch.push(element);
+          }else{
+              return newMatch;
+          }
+      }
+          );
+     return match ? newMatch : undefined;
   }
+  
+}
+
+console.log(findMatching(drivers,dname));
   
   // From the test we need to define fuzzyMatch function
-  function fuzzyMatch(drivers, letters) {
-    return drivers.filter(driver => driver.toLowerCase().startsWith(letters.toLowerCase()));
+  function fuzzyMatch(drivers,stringChars){
+    for (let i=0; i < drivers.length;){
+        const sameChars = [];
+        const nameChars = drivers.filter(element => {
+            if (element.slice(0,2) === stringChars){
+                sameChars.push(element);
+            }else{
+                return sameChars;
+            }
+  
+        })
+        return nameChars ? sameChars : undefined;
+    }
+  
   }
+  
+  
+  console.log(fuzzyMatch(drivers,dname));
   
   // From the test we need to definematchName function
-  function matchName(drivers, name) {
-    return drivers.filter(driver => driver.name === name);
+  function matchName(drivers,nameString){
+    for (let i=0; i < drivers.length;){
+        const driversName = [];
+        const dname = drivers.filter(element =>{
+            if (element.name === nameString){
+                driversName.push(element);
+            }else{
+                return driversName;
+            }
+        })
+        return dname ? driversName : undefined;
+    }
   }
   
+  console.log(matchName(drivers,nameString));
+  
+
+
+
